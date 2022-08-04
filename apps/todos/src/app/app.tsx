6d0f19a0feeb88809bc5ddddbb648f1770cb6590
Brian Chege Gichuhi <brianchegegichuhi@gmail.com>
2022-08-04 10:11:15 -0400

    adding backend app

--- apps/todos/src/app/app.tsx
+++ apps/todos/src/app/app.tsx
@@ -1,12 +1,44 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import { useState, useEffect } from 'react';
+
+interface Todo {
+  title: string;
+}
 
 export function App() {
+  const [todos, setTodos] = useState<Todo[]>([
+    { title: 'Learn TypeScript' },
+    { title: 'Learn React' },
+  ]);
+
+  function addTodo() {
+    fetch('/api/addTodo', {
+      method: 'POST',
+      body: '',
+    })
+      .then((response) => response.json())
+      .then((newTodo) => {
+        setTodos([...todos, newTodo]);
+      })
+  }
+
+  useEffect(() => {
+    fetch('/api/todos')
+      .then((response) => response.json())
+      .then(setTodos);
+  }, [])
+  
   return (
     <>
-      <NxWelcome title="todos" />
-      <div />
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li className={'todo'}>{todo.title}</li>
+        ))}
+      </ul>
+      <button id={'add-todo'} onClick={addTodo}>
+        Add Todo
+      </button>
     </>
   );
 }
